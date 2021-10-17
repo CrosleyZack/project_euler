321636129d3b6667272c04663b633a2df8f5f88e
crosleyzack <crosleyzack@gmail.com>
2021-10-17 13:05:12 -0600

    Added solution to problem 3.

---
+++ largest_factor_3.jl
@@ -0,0 +1,34 @@
+# PROBLEM 3 PROJECT EULER
+# https://projecteuler.net/problem=3
+
+# Check if a number is a multiple of another number.
+function is_multiple(number::Integer, divisor::Integer)
+    return number % divisor == 0
+end
+
+function largest_possible_factor(number::Integer)
+    return floor(sqrt(number))
+end
+
+function is_prime(number::Integer)
+    # Check first if this is a multiple of two, as that
+    #   lets us skip a lot of numbers
+    if is_multiple(number, 2)
+        return true
+    end
+    # Get largest possible factor, floor(sqrt(number))
+    maximum = sqrt(number)
+    if isinteger(maximum)
+        # We already have our answer! Hurray!
+        return true
+    else
+        maximum = Integer(floor(maximum))
+    end
+    # Check if any of the odd values from 3 to maximum are a factor.
+    return !any(is_multiple(number, x) for x=3:2:maximum)
+end
+
+function largest_factor(number::Integer)
+    maximum = Integer(largest_possible_factor(number))
+    return first(x for x=reverse(3:2:maximum) if (is_multiple(number, x) && is_prime(x)))
+end
